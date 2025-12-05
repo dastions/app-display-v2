d5e30125ab850a0654e6f2adcd8bff922f26854f
Julián Liberatore <julianLib1997@gmail.com>
2025-12-05 15:48:11 +0100

    Add README.md

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -5,7 +5,7 @@ import { toaster } from 'evergreen-ui';
 
 const SocketContext = createContext({ socket: null });
 
-const socketUrl = process.env.SOCKET_URL || 'http://localhost:4000';
+const socketUrl = process.env.SOCKET_URL || 'http://192.168.1.5:4000';
 
 console.log(`SOCKET_URL: ${process.env.SOCKET_URL}`);
 console.log(`Socket: ${socketUrl}`);
